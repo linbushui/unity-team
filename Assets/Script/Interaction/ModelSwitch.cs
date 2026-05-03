6e060036f952c948b2b82c0942d8582069ba8174
Nautiluls <aprilswind333@gmail.com>
2026-05-03 19:43:41 +0100

    拼动画

--- Assets/Script/Interaction/ModelSwitch.cs
+++ Assets/Script/Interaction/ModelSwitch.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
-public class ModelSwitch
+public class ModelSwitcher : MonoBehaviour
 {
-    
+    public GameObject enterModel;   // 带帽子 / 入场模型
+    public GameObject idleModel;    // 普通待机模型
+
+    void Start()
+    {
+        // 一开始展示入场模型
+        enterModel.SetActive(true);
+        idleModel.SetActive(false);
+    }
+
+    // 动画事件会调用这方法
+    public void SwitchToIdleModel()
+    {
+        enterModel.SetActive(false);
+        idleModel.SetActive(true);
+
+        // 顺便切动画状态
+        GetComponent<Animator>().Play("Idle");
+    }
 }
